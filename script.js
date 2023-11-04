d99c32b21598aca4d429f820a3e4ebb703b25083
yuriiprots <uraprots7849@gmail.com>
2023-11-04 18:39:59 +0200

    create the function playRound()

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ function getComputerChoice() {
   }
   return computer_choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  let result;
+
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+  ) {
+    console.log("You Won! " + playerSelection + " beats " + computerSelection);
+    result = 1;
+  }
+  if (
+    (playerSelection == "rock" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "paper")
+  ) {
+    console.log(
+      "It's a tie! " + playerSelection + " ties with " + computerSelection
+    );
+  }
+  if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "scissors" && computerSelection == "rock") ||
+    (playerSelection == "paper" && computerSelection == "scissors")
+  ) {
+    console.log(
+      "You Lost! " + playerSelection + " is beaten by " + computerSelection
+    );
+    result = 0;
+  }
+  return result;
+}
